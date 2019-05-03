0190ebc7ef4fdb3c9deaa17ec4ccc2c548830a5b
mokka86ccoffee <mokka86ccoffee@gmail.com>
2019-05-03 20:23:34 +0300

    ...

---
+++ src/api-commands.js
@@ -0,0 +1,14 @@
+/*
+
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => response.json())
+  .then(json => console.log(json))
+
+/posts	100 posts
+/comments	500 comments
+/albums	100 albums
+/photos	5000 photos
+/todos	200 todos
+/users	10 users
+
+*/
--- src/index.js
+++ src/index.js
@@ -2,36 +2,68 @@ import './index.scss'
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import DevTools from 'mobx-react-devtools';
-import { observable, decorate, configure, action, computed } from 'mobx';
+import { observable, decorate, configure, action, computed, autorun, runInAction, when  } from 'mobx';
 import { observer } from 'mobx-react';
 
 configure({ enforceActions: 'observed' })
 
-const counterState = observable({
-  count: 0,
+class counterState {
+  count = 0
+  fromFetch = []
 
   increment() {
     this.count++
-  },
+  }
 
   decrement() {
     this.count--
-  },
+  }
 
   get getCounter(){
     return this.count
   }
   
-},{
+}
+
+const store = decorate(counterState,{
+  count: observable,
   increment: action,
   decrement: action,
   getCounter: computed
-});
+})
+
+const appStore = new store()
+
+autorun(reaction => {
+  console.log(appStore.count)
+  console.log('in autorun')
+  
+  fetch('https://jsonplaceholder.typicode.com/todos')
+  .then( data => data.json() )
+  .then( data => console.log(data) )
+},{
+  name: 'autorun first one',
+  delay: 3000
+})
+
+when(
+  () => appStore.count > 5,
+  () => console.log('when worked')
+)
 
 @observer class Counter extends Component {
 
+  state = {
+    insideCounter: 0
+  }
+
   handleIncrement = () => this.props.store.increment()
   handleDecrement = () => this.props.store.decrement()
+
+  componentDidMount(){
+  }
+
+  handleIncrementWOStore = () => this.setState({ insideCounter: this.state.insideCounter + 1 })
 
   render() {
       return (
@@ -40,9 +72,11 @@ const counterState = observable({
           <h1>{this.props.store.getCounter}</h1>
           <button onClick={this.handleDecrement}>-1</button>
           <button onClick={this.handleIncrement}>+1</button>
+          <h2>{ this.state.insideCounter }</h2>
+          <button onClick={this.handleIncrementWOStore}>+1</button>
       </div>
       );
   }
 }
 
-ReactDOM.render(<Counter store={ counterState } />, document.getElementById('root'));
+ReactDOM.render(<Counter store={ appStore } />, document.getElementById('root'));
