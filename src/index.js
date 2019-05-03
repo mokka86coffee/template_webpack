9b877c78525de1293a1cc5e266d780e6066cd6e3
mokka86ccoffee <mokka86ccoffee@gmail.com>
2019-05-03 10:51:54 +0300

    ...

--- src/index.js
+++ src/index.js
@@ -2,38 +2,47 @@ import './index.scss'
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import DevTools from 'mobx-react-devtools';
-import { observable, decorate } from 'mobx';
+import { observable, decorate, configure, action, computed } from 'mobx';
 import { observer } from 'mobx-react';
+
+configure({ enforceActions: 'observed' })
 
 const counterState = observable({
   count: 0,
+
+  increment() {
+    this.count++
+  },
+
+  decrement() {
+    this.count--
+  },
+
+  get getCounter(){
+    return this.count
+  }
+  
+},{
+  increment: action,
+  decrement: action,
+  getCounter: computed
 });
-
-counterState.increment = function() {
-  this.count++
-}
-
-counterState.decrement = function() {
-  this.count--
-}
 
 @observer class Counter extends Component {
 
-    @observable count = 0;
+  handleIncrement = () => this.props.store.increment()
+  handleDecrement = () => this.props.store.decrement()
 
-    handleIncrement = () => { this.count++ };
-    handleDecrement = () => { this.count-- };
-
-    render() {
-        return (
-        <div className="App">
-            <DevTools />
-            <h1>{this.count}</h1>
-            <button onClick={this.handleDecrement}>-1</button>
-            <button onClick={this.handleIncrement}>+1</button>
-        </div>
-        );
-    }
+  render() {
+      return (
+      <div className="App">
+          <DevTools />
+          <h1>{this.props.store.getCounter}</h1>
+          <button onClick={this.handleDecrement}>-1</button>
+          <button onClick={this.handleIncrement}>+1</button>
+      </div>
+      );
+  }
 }
 
 ReactDOM.render(<Counter store={ counterState } />, document.getElementById('root'));
