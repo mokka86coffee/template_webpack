861cdf66ce329ece111b9e605de8dee919d43e31
mokka86ccoffee <mokka86ccoffee@gmail.com>
2019-05-03 21:37:30 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import './index.scss'
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+
+import axios from 'axios'
+
 import DevTools from 'mobx-react-devtools';
 import { observable, decorate, configure, action, computed, autorun, runInAction, when  } from 'mobx';
 import { observer } from 'mobx-react';
@@ -11,8 +14,11 @@ class counterState {
   count = 0
   fromFetch = []
 
-  increment() {
+  async increment () {
     this.count++
+    let result = await axios('https://jsonplaceholder.typicode.com/todos')
+    console.log(result.data)
+    this.fromFetch = result.data
   }
 
   decrement() {
@@ -38,9 +44,6 @@ autorun(reaction => {
   console.log(appStore.count)
   console.log('in autorun')
   
-  fetch('https://jsonplaceholder.typicode.com/todos')
-  .then( data => data.json() )
-  .then( data => console.log(data) )
 },{
   name: 'autorun first one',
   delay: 3000
@@ -51,7 +54,8 @@ when(
   () => console.log('when worked')
 )
 
-@observer class Counter extends Component {
+@observer 
+class Counter extends Component {
 
   state = {
     insideCounter: 0
@@ -68,12 +72,13 @@ when(
   render() {
       return (
       <div className="App">
-          <DevTools />
-          <h1>{this.props.store.getCounter}</h1>
-          <button onClick={this.handleDecrement}>-1</button>
-          <button onClick={this.handleIncrement}>+1</button>
-          <h2>{ this.state.insideCounter }</h2>
-          <button onClick={this.handleIncrementWOStore}>+1</button>
+        <DevTools />
+        <h1>{this.props.store.getCounter}</h1>
+        <h2>{ this.props.store.fromFetch.length ? this.props.store.fromFetch[0].title : 'waiting...' }</h2>
+        <button onClick={this.handleDecrement}>-1</button>
+        <button onClick={this.handleIncrement}>+1</button>
+        <h2>{ this.state.insideCounter }</h2>
+        <button onClick={this.handleIncrementWOStore}>+1</button>
       </div>
       );
   }
