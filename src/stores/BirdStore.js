bfb494a8afef8c15e029a35a3c126ce39149f059
mokka86ccoffee <mokka86ccoffee@gmail.com>
2019-05-04 12:54:37 +0300

    ...

--- src/stores/BirdStore.js
+++ src/stores/BirdStore.js
@@ -1,7 +1,7 @@
 import { decorate, observable, computed, flow, action } from 'mobx'
 
 const store = new decorate(class {
-    birds = ['chicken', 'sparrow']
+    birds = ['chicken', 'sparrow', 'smth',]
 
     get getBirds(){
         return this.birds
