59dd4ccf718d1274ee9515b4b59869f5b25ac5ce
mokka86ccoffee <mokka86ccoffee@gmail.com>
2019-05-11 01:01:00 +0300

    commit

---
+++ src/sandbox.js
@@ -0,0 +1,59 @@
+const getInf = () => ({
+    mas: [1,2,3],
+    then(onFulfilled, onRejected) {
+        if (this.mas.length) {
+            onFulfilled(this.mas.shift())
+        } else {
+            onRejected('aa')
+        }
+    }
+});
+
+
+
+(async () => {
+
+    const m = getInf();
+    console.log(m)
+    try {
+        console.log(await m);
+        console.log(await m);
+        console.log(await m);
+        console.log(await m);
+    } catch (err) {
+        console.log(err);
+    }
+})();
+
+let b = {
+    a: 'a',
+    toString(){
+        return 'str'
+    },
+    valueOf(){
+        return 1
+    }
+}
+
+let str = b + 'a'
+let obj = { [b]: 'a'}
+let numb = +b
+
+console.log(b)
+console.log(str)
+console.log(obj)
+console.log(numb)
+
+function add(a){
+    let res = a;
+    let func = b => {
+        res = a + b;
+        return func;
+    }
+
+    func.valueOf = () => res;
+    func.toString = () => 'res';
+    return func;
+}
+
+console.log(add(2))
