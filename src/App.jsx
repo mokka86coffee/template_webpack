e637e9acb99fd82715e601c1a4e7de89260cadbf
mokka86ccoffee <mokka86ccoffee@gmail.com>
2019-05-11 13:03:19 +0300

    eslint added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import styles from './App.scss';
+import styles from "./App.scss";
 
 const App = () => {
-    return React.createElement(
-        'div',
-        {
-            id: 'id',
-            onClick: () => console.log(this)
-        },
-        React.createElement('h1',{},'New app')
-    );
-}
+  return React.createElement(
+    "div",
+    {
+      id: "id",
+      onClick: () => console.log(this)
+    },
+    [
+      React.createElement("h1", { key: 1 }, "New app"),
+      React.createElement("h2", { key: 2 }, "New app"),
+      React.createElement("h2", { key: 3 }, "New app")
+    ]
+  );
+};
 
 export default App;
