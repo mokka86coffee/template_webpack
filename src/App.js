f82d5ab935c94d61534bdcca651f728c15527434
mokka86ccoffee <mokka86ccoffee@gmail.com>
2019-05-11 17:55:53 +0300

    'React.memo' added

--- src/App.js
+++ src/App.js
@@ -38,40 +38,34 @@ class App extends Component {
 
 export default App;
 
-class Header extends React.PureComponent {
-  render() {
-    console.log("Header rendered");
-    return (
-      <header>
-        <h1>Header</h1>
-        <p> Нажато {this.props.count} раз</p>
-      </header>
-    );
-  }
-}
+var Header = React.memo(({ count }) => {
+  console.log("Header rendered");
+  return (
+    <header>
+      <h1>Header</h1>
+      <p> Нажато {count} раз</p>
+    </header>
+  );
+});
 
-class Main extends React.PureComponent {
-  render() {
-    console.log("Main rendered");
-    return (
-      <main>
-        <h2>Main</h2>
-        <button onClick={this.props.setCount}>+1</button>
-      </main>
-    );
-  }
-}
+var Main = React.memo(({ setCount }) => {
+  console.log("Main rendered");
+  return (
+    <main>
+      <h2>Main</h2>
+      <button onClick={setCount}>+1</button>
+    </main>
+  );
+});
 
-class Footer extends React.PureComponent {
-  render() {
-    console.log("Footer rendered");
-    return (
-      <footer>
-        <h3>Footer</h3>
-      </footer>
-    );
-  }
-}
+var Footer = React.memo(() => {
+  console.log("Footer rendered");
+  return (
+    <footer>
+      <h3>Footer</h3>
+    </footer>
+  );
+});
 
 function Hook(props) {
   const [count, setCount] = useState(0);
