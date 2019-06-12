9d5c3c5497897a6cf0a6ace9636dc26ab86dff3c
mokka86ccoffee <mokka86ccoffee@gmail.com>
2019-06-12 16:20:30 +0300

    updated

--- src/utils/compose.js
+++ src/utils/compose.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import type {Component} from 'react';
+
 export default  (...funcs: Array<Function>) => 
                     (Component: Component): Component => 
                         funcs.reduceRight((component, fn) => fn(component), Component);
